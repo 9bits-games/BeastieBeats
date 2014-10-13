5aaa589bf9ac0b142f0810165d7b05cefab5b254
Oliver Perez Camargo <oliver.a.perez.c@gmail.com>
2014-10-13 17:23:13 -0500

    Fix: Archivos no agregados del commit anterior

---
+++ Assets/scripts/Command.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System;
+
+public class Command
+{
+    protected bool executed;
+    public bool Executed { get {return executed;} }
+
+    public Command()
+    {
+        executed = false;
+    }
+
+    public virtual void execute() {
+        executed = true;
+    }
+}
+
+public class PlayNoteCommand : Command
+{
+    public Note Note { get; private set; }
+
+    public PlayNoteCommand(Note note) : base() {
+        this.Note = note;
+    }
+
+    public override void execute() {
+        base.execute();
+
+        Debug.Log("Played " + this.Note.Name);
+    }
+}
