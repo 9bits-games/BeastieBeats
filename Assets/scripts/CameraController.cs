624e6cb4391cf41f3be8d28f4259a2d9f839d38a
Oliver Perez Camargo <oliver.a.perez.c@gmail.com>
2014-10-14 11:03:55 -0500

    Add: Particulas de sonido para las cornetas
    Add: Movimiento de camara

---
+++ Assets/scripts/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour9Bits {
+
+    public float wL = 2f;
+    public float wS = 0.1f;
+    public float hL = 3.2f;
+    public float hS = 0.05f;
+
+    public Transform player;
+    public Vector3 lookAtOffset;
+
+    private Vector3 initPos;
+
+	// Use this for initialization
+	void Start () {
+        initPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (player) transform.LookAt(player.position + lookAtOffset);
+        
+        transform.position = initPos + new Vector3(
+            Mathf.Sin(Time.time / wL) * wS,
+            Mathf.Sin(Time.time / hL) * hS,
+            0
+        );
+	}
+}
