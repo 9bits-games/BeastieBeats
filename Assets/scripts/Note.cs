628cd095424069c4fb2d795d07f2572ff4f25dd2
Oliver Perez Camargo <oliver.a.perez.c@gmail.com>
2014-10-28 11:45:53 -0500

    Fix: Archivos faltantes del commit anterior

--- Assets/scripts/Note.cs
+++ Assets/scripts/Note.cs
@@ -9,9 +9,9 @@ public class Note {
     public static Note DO = new Note {Name="Do", Color = Color.red};
     public static Note RE = new Note {Name="Re", Color = Color.blue};
     public static Note MI = new Note {Name="Mi", Color = Color.yellow};
-    public static Note FA = new Note {Name="Fa", Color = Color.green};
+    public static Note FA = new Note {Name="Fa", Color = Color.gray};
     public static Note SOL = new Note {Name="Sol", Color = Color.green};
-    public static Note LA = new Note {Name="La", Color = Color.magenta};
+    public static Note LA = new Note {Name="La", Color = Color.cyan};
     public static Note Si = new Note {Name="Si", Color = Color.magenta};
 
     public string Name;
