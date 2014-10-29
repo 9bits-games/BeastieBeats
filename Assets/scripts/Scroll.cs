81893635d042135c27505fe146560bd2d84cd04f
rimerz <rimerz@gmail.com>
2014-10-29 13:45:14 -0500

    Mi primer aporte en 9 Bits Pantalla de Instrucciones.

---
+++ Assets/scripts/Scroll.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scroll : MonoBehaviour {
+
+	public bool IniciarEnMovimiento = false;
+	public float velocidad = 0f;
+	private bool enMovimiento = false;
+	private float tiempoInicio = 0f;
+
+	// Use this for initialization
+	void Start () {
+		if (IniciarEnMovimiento) {
+			enMovimiento = true;
+		}
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		if(enMovimiento){
+			renderer.material.mainTextureOffset = new Vector2(((Time.time - tiempoInicio) * velocidad) % 1, 0);
+		
+		}
+	}
+}
